2ab19a358edbccd5e6eef1fa36439b0fbcbc2147
Angel de Jesus Rufino Mendoza <230499@utxicotepec.edu.mx>
2025-01-13 15:11:19 -0600

    Agregación de formato de segundos en kla pracTICA

--- app.js
+++ app.js
@@ -25,16 +25,24 @@ app.use((req,res, next)=>{
 });
 
 
+app.get('/login/:User',(req,res)=>{
+    req.session.User=req.params.User;
+    res.send("Usuario guardado");
+})
+
 //Ruta para mostrar la información de la sesión
 app.get('/session',(req,res)=>{
     if(req.session){
+        const User = req.session.User;
         const sessionId = req.session.id;
         const createdAt = req.session.createdAt;
         const lastAccess = req.session.lastAccess;
-        const sessionDuration = (new Date() - createdAt)/1000; //Duración de la sesión en segundos
+        const sessionDuration = (new Date() - new Date(createdAt))/1000; //Duración de la sesión en segundos
+        console.log(`La duración de la sesión es de ${sessionDuration} segundos.`);
         
         res.send(`
             <h1>Detalles de la sesion</h1>
+            <p><strong>Usuario:</strong>${User}</p>
             <p><strong>ID de sesión:</strong>${sessionId}</p>
             <p><strong>Fecha de creación de la sesión:</strong>${createdAt}</p>
             <p><strong>último acceso:</strong>${lastAccess}</p>
